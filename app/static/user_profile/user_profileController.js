a6a58b0027c107e114d43b77c0ab545c8b8737f8
Corey Weber <coreyweber11@gmail.com>
2016-01-20 16:20:03 -0400

    Fixed more of brandos shitty coding

--- app/static/user_profile/user_profileController.js
+++ app/static/user_profile/user_profileController.js
@@ -1,7 +1,6 @@
 /**
- * Created by Brandon on 1/20/2016.
+ * Created by Corey Weber on 2016-01-20.
  */
-
 angular.module('dashboard.controllers').controller('user_profileController', ['$scope', '$http', function ($scope, $http) {
 
     console.log("user_profile Page");
