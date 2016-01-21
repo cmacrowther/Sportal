af492e0a72fa5384f5de106ae9af3d11e0c63893
Brandon <brbanks@upei.ca>
2016-01-21 15:50:09 -0400

    Fixed user_profile controller

--- app/static/user_profile/user_profileController.js
+++ app/static/user_profile/user_profileController.js
@@ -2,7 +2,7 @@
  * Created by Brandon on 1/20/2016.
  */
 
-angular.module('dashboard.controllers',[]).controller('user_profileController', ['$scope', '$http', function ($scope, $http) {
+angular.module('dashboard.controllers').controller('user_profileController', ['$scope', '$http', function ($scope, $http) {
 
     console.log("user_profil Page");
 
