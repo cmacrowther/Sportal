54c9fcbb0174dce2b5b9fb74ca5dffbb813b37cb
Brandon <brbanks@upei.ca>
2016-01-21 16:34:27 -0400

    Created Play Folder and HTML/Controller files

---
+++ app/static/play/playController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Brandon on 1/21/2016.
+ */
+angular.module('dashboard.controllers').controller('playController', ['$scope', '$http', function ($scope, $http) {
+
+    console.log("Play Page");
+
+
+
+}]);
