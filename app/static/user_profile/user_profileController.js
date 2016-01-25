4e70bde234f0c982c92f3281d2570369a9bff124
Brandon <brbanks@upei.ca>
2016-01-25 14:21:22 -0400

    Made variables be shown if already inputted.

--- app/static/user_profile/user_profileController.js
+++ app/static/user_profile/user_profileController.js
@@ -2,18 +2,44 @@
  * Created by Brandon on 1/20/2016.
  */
 
-angular.module('dashboard.controllers').controller('user_profileController', ['$scope', '$http', function ($scope, $http) {
+angular.module('dashboard.controllers').controller('user_profileController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
 
     console.log("user_profil Page");
 
     $("#user_creation_form").validator();
 
     //Initializes the date time picker plugin.
-	$('#birth').datetimepicker({
+    $('#birth').datetimepicker({
         format: 'MM/DD/YYYY',
         viewMode: 'years'
     });
 
-    
+    if ($rootScope.userObject.first_name == "" || $rootScope.userObject.first_name == undefined) {
+        $scope.first_name_value = true;
+    }
+    else {
+        $scope.first_name_value = false;
+        $scope.first_name = $rootScope.userObject.first_name;
+    }
+    if ($rootScope.userObject.last_name == "" || $rootScope.userObject.last_name == undefined) {
+        $scope.last_name_value = true;
+    }
+    else {
+        $scope.last_name_value = false;
+        $scope.last_name = $rootScope.userObject.last_name;
+    }
+    if ($rootScope.userObject.email == "" || $rootScope.userObject.email == undefined) {
+        $scope.email_value = true;
+    }
+    else {
+        $scope.email_value = false;
+        $scope.email = $rootScope.userObject.email;
+    }
+
+
+    $scope.edit_first_name = function(){
+        $scope.first_name_value = true;
+    }
+
 
 }]);
