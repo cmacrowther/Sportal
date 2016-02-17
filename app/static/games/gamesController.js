923fc6eb217456449f2333ce18ea0c2e71d04da7
tdoucett <tdoucett@uwaterloo.ca>
2016-02-17 16:16:00 -0400

    more games functionality

--- app/static/games/gamesController.js
+++ app/static/games/gamesController.js
@@ -27,6 +27,22 @@ angular.module('dashboard.controllers').controller('gamesController', ['$scope',
     		for(var i = 0; i < data.length; i++) {
 	    		if (data[i].complete == 1){
 	    			$scope.past_games.push(data[i]);
+	    			if (data[i].winner_id == $rootScope.userObject.id) {
+	    				if (data[i].score_1 > data[i].score_2) {
+	    					$scope.results = "WIN " + data[i].score_1 + "-" + data[i].score_2;
+	    				}
+	    				else {
+	    					$scope.results = "WIN " + data[i].score_2 + "-" + data[i].score_1;
+	    				}
+	    			}
+	    			else {
+	    				if (data[i].score_1 > data[i].score_2) {
+	    					$scope.results = "LOSS " + data[i].score_1 + "-" + data[i].score_2;
+	    				}
+	    				else {
+	    					$scope.results = "LOSS " + data[i].score_2 + "-" + data[i].score_1;
+	    				}
+	    			}
 	    			$scope.no_past_games = "";
 	    		}
 	    		else {
@@ -42,7 +58,7 @@ angular.module('dashboard.controllers').controller('gamesController', ['$scope',
         
         $http.get("/api/sport/" + item.sport_id)
         .success(function(data){
-            item.sport_id = data.name;
+            item.sport_name = data.name;
         })
 
     }
@@ -53,7 +69,61 @@ angular.module('dashboard.controllers').controller('gamesController', ['$scope',
         .success(function(data){
             item.opponent = data.first_name + " " + data.last_name;
         })
+    }
 
+    $scope.setModal = function (item) {
+    	$scope.modalObject = item;
+    }
+
+    $http.get("/api/sport")
+    .success(function (data) {
+        console.log(data);
+        $scope.allSports = data.objects;
+    })
+
+    $scope.updateInfo = function () {
+            
+        console.log("Updating Info.");
+
+        $scope.modalObject.facility_id = $scope.facility;
+        $scope.modalObject.date = $scope.date;
+        $scope.modalObject.time = $scope.time;
+
+        $http.put("api/match/" + $scope.modalObject.id, $scope.modalObject);
+    }
+
+    $scope.getWinner = function() {
+    	if ($scope.your_score > $scope.opponent_score) {
+    		$scope.game_results = "You Won! :)";
+			$scope.modalObject.winner_id = $rootScope.userObject.id;
+    	}
+    	else if ($scope.your_score < $scope.opponent_score) {
+    		$scope.game_results = "You Lost! :(";
+    		$scope.modalObject.winner_id = $scope.modalObject.opponent.id;
+    	}
+    	else {
+    		$scope.game_results = "You Tied!";
+    		$scope.modalObject.winner_id = null;
+    	}
+    }
+
+    $scope.finishGame = function() {
+    	if ($rootScope.userObject.id == $scope.modalObject.player1_id) {
+    		$scope.modalObject.score_1 = $scope.your_score;
+    		$scope.modalObject.score_2 = $scope.opponent_score;
+    	}
+    	else {
+    		$scope.modalObject.score_2 = $scope.your_score;
+    		$scope.modalObject.score_1 = $scope.opponent_score;
+    	}
+
+    	console.log($scope.modalObject);
+
+    	$http.put("api/match/" + $scope.modalObject.id, $scope.modalObject)
+    	.success(function(){
+    		$scope.gamesip.splice($scope.modalObject, 1);
+    		$scope.past_games.push($scope.modalObject);
+    	})
     }
 
 }]);
