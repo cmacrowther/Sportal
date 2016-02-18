0273bd12f57d7c2f1eae06404ea67949a4b7d570
Brandon <brbanks@upei.ca>
2016-02-18 16:02:51 -0400

    Created timeouts for result messages that are displayed

--- app/static/admin/adminController.js
+++ app/static/admin/adminController.js
@@ -1,47 +1,53 @@
 /**
  * Created by Thomas Doucette on 2016-01-21.
  */
-angular.module('dashboard.controllers').controller('adminController', ['$scope', '$http', function ($scope, $http) {
+angular.module('dashboard.controllers').controller('adminController', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
 
-	//Initialize validators
-	$('#sportForm').validator();
-	$('#facilityForm').validator();
+    //Initialize validators
+    $('#sportForm').validator();
+    $('#facilityForm').validator();
 
     //create a new sport
-    $scope.createSport = function() {
+    $scope.createSport = function () {
         //resetting result variables
         $scope.createFacilityResult = "";
         $scope.createSportResult = "";
 
-	    //sending the sport to the database
-		$http.post("api/sport", {
-    		number_of_members: $scope.members_required,
-    		number_of_teams: $scope.teams_required,
-    		name: $scope.sport_name,
-    		draw_flag: 1
-		})
-		.success(function(){
-			console.log($scope.sport_name);
-    		console.log($scope.members_required);
-    		console.log($scope.teams_required);
-			console.log("Sport Logged into DB");
-			$scope.createSportResult = "Sport Successfully Added!";
-		})
+        //sending the sport to the database
+        $http.post("api/sport", {
+                number_of_members: $scope.members_required,
+                number_of_teams: $scope.teams_required,
+                name: $scope.sport_name,
+                draw_flag: 1
+            })
+            .success(function () {
+                console.log($scope.sport_name);
+                console.log($scope.members_required);
+                console.log($scope.teams_required);
+                console.log("Sport Logged into DB");
+                $scope.createSportResult = "Sport Successfully Added!";
+                $timeout(function () {
+                    $scope.createSportResult = "";
+                }, 3000);
+            })
     };
 
-	//Create a new facility
-    $scope.createFacility = function() {
-    	//sending the facility to the database
-		$http.post("api/facility", {
-			name: $scope.facility_name,
-			address: $scope.facility_address
-		})
-		.success(function(){
-    		console.log($scope.facility_name);
-    		console.log($scope.facility_address);
-			console.log("Facility Logged into DB");
-			$scope.createFacilityResult = "Facility Successfully Added!";
-		})
+    //Create a new facility
+    $scope.createFacility = function () {
+        //sending the facility to the database
+        $http.post("api/facility", {
+                name: $scope.facility_name,
+                address: $scope.facility_address
+            })
+            .success(function () {
+                console.log($scope.facility_name);
+                console.log($scope.facility_address);
+                console.log("Facility Logged into DB");
+                $scope.createFacilityResult = "Facility Successfully Added!";
+                $timeout(function () {
+                    $scope.createFacilityResult = "";
+                }, 3000);
+            })
     };
 }]);
 
