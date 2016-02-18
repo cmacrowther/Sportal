43ce7e600b31cdef84957bd016cc52b393053e1f
Brandon <brbanks@upei.ca>
2016-02-18 10:40:32 -0400

    fixed put method for games

--- app/static/games/gamesController.js
+++ app/static/games/gamesController.js
@@ -109,21 +109,41 @@ angular.module('dashboard.controllers').controller('gamesController', ['$scope',
 
     $scope.finishGame = function () {
         $http.get("api/match/" + $scope.modalObject.id)
-        .success(function (data) {
-            if ($rootScope.userObject.id == $scope.modalObject.player1_id) {
-                data.score_1 = $scope.your_score;
-                data.score_2 = $scope.opponent_score;
-            }
-             else {
-               data.score_2 = $scope.your_score;
-               data.score_1 = $scope.opponent_score;
-            }
-            $http.put("api/match/" + data.id, data)
             .success(function (data) {
-                console.log("Testing if we get into success.");
-                $scope.gamesip.splice(data, 1);
-                $scope.past_games.push(data);
+                if ($rootScope.userObject.id == $scope.modalObject.player1_id) {
+                    data.score_1 = $scope.your_score;
+                    data.score_2 = $scope.opponent_score;
+                    data.winner_id = $scope.modalObject.winner_id;
+                }
+                else {
+                    data.score_2 = $scope.your_score;
+                    data.score_1 = $scope.opponent_score;
+                    data.winner_id = $scope.modalObject.winner_id;
+                }
+                data.complete = 1;
+                $http.put("api/match/" + data.id, data)
+                    .success(function (data) {
+                        console.log("Testing if we get into success.");
+                        if (data.winner_id == $rootScope.userObject.id) {
+                            if (data.score_1 > data.score_2) {
+                                $scope.results = "WIN " + data.score_1 + "-" + data.score_2;
+                            }
+                            else {
+                                $scope.results = "WIN " + data.score_2 + "-" + data.score_1;
+                            }
+                        }
+                        else {
+                            if (data.score_1 > data.score_2) {
+                                $scope.results = "LOSS " + data.score_1 + "-" + data.score_2;
+                            }
+                            else {
+                                $scope.results = "LOSS " + data.score_2 + "-" + data.score_1;
+                            }
+                        }
+                        $scope.gamesip.splice(data, 1);
+                        $scope.past_games.push(data);
+                        $scope.no_past_games = "";
+                    });
             });
-        });
     };
 }]);
