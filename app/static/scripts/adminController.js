946127b0269665996b8a278ea22f5d96539d1605
Brandon <brbanks@upei.ca>
2016-04-04 19:18:58 -0300

    Added comments and corrected formatting

--- app/static/scripts/adminController.js
+++ app/static/scripts/adminController.js
@@ -1,5 +1,8 @@
 /**
  * Created by Brandon Banks, Corey Weber, Colin Crowther, & Thomas Doucette on 2016-01-21.
+ *
+ * adminController - Includes functions to create a new sport for the Sportal platform users or
+ * to create a new facility.
  */
 
 angular.module('dashboard.controllers').controller('adminController', ['$scope', '$timeout', '$http', '$rootScope', function ($scope, $timeout, $http, $rootScope) {
@@ -10,7 +13,7 @@ angular.module('dashboard.controllers').controller('adminController', ['$scope',
 
     $rootScope.page_name = "Admin";
 
-    //creates a new sport
+    /* Creates a new sport so that users can be matched with that sport */
     $scope.createSport = function () {
         //resetting result variables
         $scope.createFacilityResult = "";
@@ -18,21 +21,20 @@ angular.module('dashboard.controllers').controller('adminController', ['$scope',
 
         //sending the sport to the database
         $http.post("api/sport", {
-                number_of_members: $scope.members_required,
-                number_of_teams: $scope.teams_required,
-                name: $scope.sport_name,
-                draw_flag: 1
-            })
-            .success(function () {
-                $scope.createSportResult = "Sport Successfully Added!";
-                //resets result variable
-                $timeout(function () {
-                    $scope.createSportResult = "";
-                }, 3000);
-            })
+            number_of_members: $scope.members_required,
+            number_of_teams: $scope.teams_required,
+            name: $scope.sport_name,
+            draw_flag: 1
+        }).success(function () {
+            $scope.createSportResult = "Sport Successfully Added!";
+            //resets result variable
+            $timeout(function () {
+                $scope.createSportResult = "";
+            }, 3000);
+        })
     };
 
-    //Creates a new facility
+    /* Creates a new facility and adds it to the sportal platform */
     $scope.createFacility = function () {
         //resetting result variables
         $scope.createFacilityResult = "";
@@ -40,16 +42,15 @@ angular.module('dashboard.controllers').controller('adminController', ['$scope',
 
         //sending the facility to the database
         $http.post("api/facility", {
-                name: $scope.facility_name,
-                address: $scope.facility_address
-            })
-            .success(function () {
-                $scope.createFacilityResult = "Facility Successfully Added!";
-                //resets result message
-                $timeout(function () {
-                    $scope.createFacilityResult = "";
-                }, 3000);
-            })
+            name: $scope.facility_name,
+            address: $scope.facility_address
+        }).success(function () {
+            $scope.createFacilityResult = "Facility Successfully Added!";
+            //resets result message
+            $timeout(function () {
+                $scope.createFacilityResult = "";
+            }, 3000);
+        })
     };
 }]);
 
