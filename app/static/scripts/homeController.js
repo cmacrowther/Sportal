67e5d11e402b34db99c3473a30d3d1a6f0f42c1b
Brandon <brbanks@upei.ca>
2016-04-04 20:43:53 -0300

    Comments & formatting

--- app/static/scripts/homeController.js
+++ app/static/scripts/homeController.js
@@ -1,13 +1,14 @@
 /**
  * Created by Brandon Banks, Corey Weber, Colin Crowther, & Thomas Doucette on 2016-01-21.
+ *
+ * homeController - Controller for home page.
  */
 
 angular.module('dashboard.controllers',[]).controller('HomeController', ['$scope', '$http','$rootScope', function ($scope, $http, $rootScope) {
 
     $rootScope.page_name = "Home";
 
-
-    //Initializes the date time picker plugin.
+    /* Initializes the date time picker plugin. */
     $('#birth').datetimepicker({
         format: 'MM/DD/YYYY',
         viewMode: 'years'
