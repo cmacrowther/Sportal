4b2ea57a7d39886985e9bc3de175a482cefad33f
Brandon <brbanks@upei.ca>
2016-04-04 20:01:17 -0300

    Comment Added to header

--- app/static/scripts/errorController.js
+++ app/static/scripts/errorController.js
@@ -1,5 +1,7 @@
 /**
  * Created by Brandon Banks, Corey Weber, Colin Crowther, & Thomas Doucette on 2016-01-21.
+ *
+ * errorController - Displays an error page with the error message.
  */
 
 angular.module('dashboard.controllers').controller('errorController', ['$scope', '$rootScope', '$http', '$routeParams', function ($scope, $rootScope, $http, $routeParams) {
