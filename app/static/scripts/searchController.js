f56e44540c284424c218938922294e37e246749e
Brandon <brbanks@upei.ca>
2016-04-04 21:12:48 -0300

    Comments & formatting.

--- app/static/scripts/searchController.js
+++ app/static/scripts/searchController.js
@@ -1,26 +1,27 @@
 /**
  * Created by Brandon Banks, Corey Weber, Colin Crowther, & Thomas Doucette on 2016-01-21.
+ *
+ * searchController - Allows users to view other users/teams profile pages by search.
  */
 
 angular.module('dashboard.controllers').controller('searchController', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
-    //Declaring Variables
+    /* Declaring Variables */
     $scope.no_users = "No Matching Users";
     $scope.no_teams = "No Matching Teams";
     $rootScope.page_name = "Search";
 
-	//Function for searching database
-	$scope.search = function() {
+    /* Searches database for other users and teams so you can view their profile pages */
+    $scope.search = function () {
 
-		var passObject = {searchTerm: $scope.searchTerm};
+        var passObject = {searchTerm: $scope.searchTerm};
 
-		$http({
-    		method: 'POST',
-    		url: 'api/user_search',
-    		headers: {'Content-Type': 'application/json'},
-    		data: JSON.stringify(passObject)
-	    })
-    	.success(function(data){
+        $http({
+            method: 'POST',
+            url: 'api/user_search',
+            headers: {'Content-Type': 'application/json'},
+            data: JSON.stringify(passObject)
+        }).success(function (data) {
             console.log(data);
             if (data == "no matching users") {
                 $rootScope.users_list = [];
@@ -30,15 +31,14 @@ angular.module('dashboard.controllers').controller('searchController', ['$scope'
                 $rootScope.users_list = data;
                 $scope.no_users = "";
             }
-        })
+        });
 
         $http({
-    		method: 'POST',
-    		url: 'api/team_search',
-    		headers: {'Content-Type': 'application/json'},
-    		data: JSON.stringify(passObject)
-	    })
-    	.success(function(data){
+            method: 'POST',
+            url: 'api/team_search',
+            headers: {'Content-Type': 'application/json'},
+            data: JSON.stringify(passObject)
+        }).success(function (data) {
             console.log(data);
             if (data == "no matching teams") {
                 $rootScope.teams_list = [];
@@ -49,15 +49,12 @@ angular.module('dashboard.controllers').controller('searchController', ['$scope'
                 $scope.no_teams = "";
             }
         })
-	}
+    };
 
-    //Used for displaying the name of the Team's Sport
-    $scope.getSport = function(item){
-        
-        $http.get("/api/sport/" + item.sport_id)
-        .success(function(data){
+    /* Used for displaying the name of the Team's Sport */
+    $scope.getSport = function (item) {
+        $http.get("/api/sport/" + item.sport_id).success(function (data) {
             item.sport_id = data.name;
         })
-
-    }
+    };
 }]);
