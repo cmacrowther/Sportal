8c64ef24ff4293709cff7cc466746786f4b4bfbd
Brandon <brbanks@upei.ca>
2016-04-04 21:09:32 -0300

    Comments & formatting

--- app/static/scripts/pusherController.js
+++ app/static/scripts/pusherController.js
@@ -1,58 +1,56 @@
 /**
  * Created by Brandon Banks, Corey Weber, Colin Crowther, & Thomas Doucette on 2016-01-21.
+ *
+ * Controller was made to test the pusher plugin.
+ * Not used.
  */
 
 angular.module('dashboard.controllers').controller('pusherTestController', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
-	//pusher testing
+    /* Pusher testing */
+    $scope.items = [];
+    $scope.test_message = "Default";
 
-	$scope.items = [];
-	$scope.test_message = "Default";
-
-	Pusher.log = function(message) {
-      if (window.console && window.console.log) {
-        window.console.log(message);
-      }
+    Pusher.log = function (message) {
+        if (window.console && window.console.log) {
+            window.console.log(message);
+        }
     };
 
     var pusher = new Pusher('56753b214ab2420a7230', {
-      encrypted: true
+        encrypted: true
     });
 
     var channel = pusher.subscribe('test_channel');
-    channel.bind('hi', function(data) {
-       $scope.items.push(data.message);
+    channel.bind('hi', function (data) {
+        $scope.items.push(data.message);
     });
 
     //sets the event_id to listen for
     $scope.setMessageType = function (id, item) {
-    	if (item == "channel") {
-    		$scope.event_id = id + "-channel";
-    		console.log($scope.event_id);
-    	}
-    	else {
-    		$scope.event_id = id + "-convo";
-    		console.log($scope.event_id);
-    	}
-    }
+        if (item == "channel") {
+            $scope.event_id = id + "-channel";
+            console.log($scope.event_id);
+        }
+        else {
+            $scope.event_id = id + "-convo";
+            console.log($scope.event_id);
+        }
+    };
 
     $scope.sendMessage = function (item) {
-    	
-    	if($scope.event_id != undefined){
-    		var passObject = {event_id: $scope.event_id, message: item};
+        if ($scope.event_id != undefined) {
+            var passObject = {event_id: $scope.event_id, message: item};
 
-	    	$http({
-	            method: 'POST',
-	            url: 'api/send_message',
-	            headers: {'Content-Type': 'application/json'},
-	            data: JSON.stringify(passObject)
-	        })
-	        .success(function (data) {
-	        	console.log("MESSAGE SENT");
-	        })
-    	}
-    	
+            $http({
+                method: 'POST',
+                url: 'api/send_message',
+                headers: {'Content-Type': 'application/json'},
+                data: JSON.stringify(passObject)
+            }).success(function (data) {
+                console.log("MESSAGE SENT");
+            })
+        }
     }
-
 
 }]);
